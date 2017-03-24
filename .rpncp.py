f45f89a4e460cbca37443d4e76ff20042d54f915
Joe <jphlmn@umich.edu>
2017-03-23 23:37:48 -0400

    updated rpn and attempted coverage in .travis.yml

--- .rpncp.py
+++ .rpncp.py
@@ -2,35 +2,36 @@
 
 import operator
 
-OPERATORS = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    '/': operator.truediv,
-    '^': operator.pow,
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,
+        '^': operator.pow,
 }
 
-
-def calculate(arg):
-    stack = list()
-    for operand in arg.split():
-        try:
-            operand = float(operand)
-            stack.append(operand)
-        except:
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            operator_fn = OPERATORS[operand]
-            result = operator_fn(arg1, arg2)
-
-            
-            stack.append(result)
+def calculate(myarg):
+        stack = list()
+        for token in myarg.split():
+                try:
+                        token = int(token)
+                        stack.append(token)
+                except ValueError:
+                        function = operators[token]
+                        arg2 = stack.pop()
+                        arg1 = stack.pop()
+                        result = function(arg1, arg2)
+                        stack.append(result)
+                print(stack)
+        if len(stack) != 1:
+                raise TypeError("Too many parameters")
         return stack.pop()
-    
 
 def main():
-    while True:
-        result = calculate(input('rpn calc> '))
-        print("Result:", result)
+        while True:
+                result = calculate(input("rpn calc> "))
+                print("Result: ", result)
+
 if __name__ == '__main__':
-    main()
+        main()
--- rpn.py
+++ rpn.py
@@ -1,36 +1,57 @@
 #!/usr/bin/env python3
-
+import sys
+import readline
 import operator
 
-OPERATORS = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    '/': operator.truediv,
-    '^': operator.pow,
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,
+        '^': operator.pow,
+        '%': operator.mod,
 }
 
-
-def calculate(arg):
-    stack = list()
-    for operand in arg.split():
-        try:
-            operand = float(operand)
-            stack.append(operand)
-        except:
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            operator_fn = OPERATORS[operand]
-            result = operator_fn(arg1, arg2)
-
-            
-            stack.append(result)
+def calculate(myarg):
+        stack = list()
+        for token in myarg.split():
+                try:
+                        token = int(token)
+                        stack.append(token)
+                except ValueError:
+                        function = operators[token]
+                        arg2 = stack.pop()
+                        arg1 = stack.pop()
+                        result = function(arg1, arg2)
+                        stack.append(result)
+               # print(stack)
+        if len(stack) != 1:
+                raise TypeError("Too many parameters")
         return stack.pop()
-    
 
 def main():
-    while True:
-        result = calculate(input('rpn calc> '))
-        print("Result:", result)
+        print("type help for information, or type exit to quit")
+        while True:
+                user = input('rpn calc> ')
+                if user == "exit":
+                        print ("exiting rpn calculator")
+                        sys.exit()
+                elif user == "help":
+                        print ("The rpn caclulator uses the form of # # operation. type operations for a list of available operations")
+                elif user == "operations":
+                        print("+: addition")
+                        print("-: subtraction")
+                        print("*: multiplication")
+                        print("/: true division")
+                        print("^: exponentiation")
+                        print("%: modulus")
+                else:
+                        try:
+                                result = calculate(user)
+                                print("Result: ", result)
+                        except:
+                                print("you must input in the form of # # operation. please try again.")
+
 if __name__ == '__main__':
-    main()
+        main()
